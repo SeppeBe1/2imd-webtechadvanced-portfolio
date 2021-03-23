57a2cedfe9ada40ecdd07164fb50a70938fcc5ea
SeppeBe1 <seppe.bellens@hotmail.nl>
2021-03-23 23:47:49 +0100

    getWeather done

--- lab4/app.js
+++ lab4/app.js
@@ -1,24 +1,26 @@
 class App {
     constructor() {
-        this.latgitude = -37.872194;
-        this.longitude = 175.68321;
-        this.getWeather;
+        this.getWeather();
     }
 
     getWeather() {
+        let latitude = -37.872194;
+        let longitude = 175.68321;
         let key = "bcbebac378114c7cbdb0982444f834cf";
-        let url = `https://api.weatherbit.io/v2.0/current?lat=${this.latgitude}&lon=${this.longitude}&key=${key}&include=hourly`;
+        let url = `https://api.weatherbit.io/v2.0/current?lat=${latitude}&lon=${longitude}&key=${key}&include=minutly`;
         fetch(url)
-            .then(response => {
-                console.log(response);
-                response.json;
+            .then((response) => {
+                return response.json();
             })
-            .then(data => {
-                console.log(data);
-            })
-            .catch(error => {
-                console.log(error);
+            .then((json) => {
+                let weather = json.data[0].weather.description;
+
+                console.log(weather);
+
             });
+        // .catch(error => {
+        //     console.log(error);
+        // });
     }
 }
 
